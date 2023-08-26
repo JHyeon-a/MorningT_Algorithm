37dc3cc2fd6a9bc3bc21936a71f3d14759879a5b
heon118 <ectmdgjs@gmail.com>
2023-08-26 15:10:41 +0900

    [LSH] BOJ_1449_S3

--- 알고리즘/00_공통문제/2023_08_5주차/LSH_BOJ_1449_S3.java
+++ 알고리즘/00_공통문제/2023_08_5주차/LSH_BOJ_1449_S3.java
@@ -18,15 +18,14 @@ public class Main {
 
 		Arrays.sort(water);
 
-		int idx = 0;
-		int cnt = 1;
+		int idx = 0; // 테이프 시작 위치
+		int cnt = 1; // 테이프 개수 - 아래 반복문으로는 마지막 물 새는 곳은 붙일 수 없으므로 1로 시작
 		for (int i = 0; i < N - 1; i++) {
-			if (water[i+1] - water[idx] >= L) {
-				cnt++;
-				idx = i+1;
+			if (water[i+1] - water[idx] >= L) { // 다음 물 새는 곳이 테이프가 닿지 않는다면?
+				cnt++; // 일단 테이프 붙여
+				idx = i+1; // 테이프를 다음 물 새는 곳부터 시작해
 			}
 		}
 		System.out.println(cnt);
-
 	}
 }
